4c6cd56f2269cb39706ad86c6f0a1e537521d179
Pedro Ulisses Pereira Castro Maia <90991139+pedroulissespu@users.noreply.github.com>
2022-04-21 10:46:41 -0300

    Add files via upload

---
+++ Kotlin_Development/src/parte1.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    var nome: String = "Pedro Ulisses Pereira Castro Maia"
+    var textovazio: String
+
+    val tamanho_calcula: Byte = 38
+    val pib = 1869000000000
+    val populacao = 211000000
+
+    println("Pib per capita : ${pib/populacao}")
+
+}
